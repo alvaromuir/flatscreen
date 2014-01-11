04bd0a9ae5f97261f821ecd01d897fd39a76f80f
Alvaro Muir <alvaromuir@gmail.com>
2014-01-11 14:32:02 -0500

    Initial Commit

---
+++ FlatScreen/FlatScreenView.h
@@ -0,0 +1,13 @@
+//
+//  FlatScreenView.h
+//  FlatScreen
+//
+//  Created by Alvaro Muir on 1/11/14.
+//  Copyright (c) 2014 muiral. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface FlatScreenView : ScreenSaverView
+
+@end
---
+++ FlatScreen/FlatScreenView.m
@@ -0,0 +1,52 @@
+//
+//  FlatScreenView.m
+//  FlatScreen
+//
+//  Created by Alvaro Muir on 1/11/14.
+//  Copyright (c) 2014 muiral. All rights reserved.
+//
+
+#import "FlatScreenView.h"
+
+@implementation FlatScreenView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
